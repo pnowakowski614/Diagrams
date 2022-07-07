0fb003b9b4ba8fa75b821624fd89087c970ae3b5
Ideekay12 <48804783+Ideekay12@users.noreply.github.com>
2022-07-07 16:15:36 +0200

    feat(): created login page

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import Diagram from "./app/views/diagram/diagram";
+import React from "react";
+import { Route } from "react-router-dom";
+import Login from "./app/views/login/login";
+import List from "./app/views/list/list";
+import Header from "./app/shared_components/header"
+
+const App = () => {
+    return(
+        <div>
+            <Header />
+            <main>
+                <Route path="/login">
+                    <Login />
+                </Route>
+                <Route path="/diagram">
+                    <Diagram />
+                </Route>
+                <Route path="/list">
+                    <List />
+                </Route>
+            </main>
+        </div>
+    )
+}
+
+export default App;
