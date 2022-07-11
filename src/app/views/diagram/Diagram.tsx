360f381b4f735cf4ccec3d2007801a40bc4ce354
Ideekay12 <48804783+Ideekay12@users.noreply.github.com>
2022-07-11 19:40:32 +0200

    feat(diagram): added inspector

--- src/app/views/diagram/Diagram.tsx
+++ src/app/views/diagram/Diagram.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './diagram.module.scss';
 import "./rappid.scss"
 import Rappid from './rappid';
@@ -7,6 +7,9 @@ import useEffectOnce from "../../helpers/useEffectOnce";
 const Diagram = () => {
     const canvas = useRef(null);
     const stencil = useRef(null);
+
+    const [isInspectorDisp, setInspectorDisp] = useState(false);
+
     useEffectOnce(() => {
         const rappidInst = new Rappid(canvas.current!, stencil.current!);
         rappidInst.init();
@@ -15,7 +18,10 @@ const Diagram = () => {
     return (
         <div className={styles.wrapper}>
             <div className={styles.stencilHolder} ref={stencil}/>
-            <div className={styles.canvas} ref={canvas}/>
+            <div className={styles.canvas} onMouseEnter={() => setInspectorDisp(true)}
+                 onMouseLeave={() => setInspectorDisp(false)} ref={canvas}>
+                {isInspectorDisp && <div className={styles.inspector}>inspector</div>}
+            </div>
         </div>
     );
 }
