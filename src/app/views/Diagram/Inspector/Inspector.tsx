7931a69c34edcc0e1431dacbb1980fcebf42665b
Ideekay12 <48804783+Ideekay12@users.noreply.github.com>
2022-09-15 17:37:01 +0200

    feat(): add port placement change option to inspector

--- src/app/views/Diagram/Inspector/Inspector.tsx
+++ src/app/views/Diagram/Inspector/Inspector.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from './inspector.module.scss';
-import { Input, List, ListSubheader } from "@mui/material";
+import { Input, MenuItem, Select, SelectChangeEvent } from "@mui/material";
 import { dia } from "@clientio/rappid";
+import { getShapeLabelWidth } from "../../../utils/rappid-utils";
+
 
 interface InspectorProps {
     elementView: dia.ElementView
@@ -9,35 +11,74 @@ interface InspectorProps {
 
 const Inspector = ({elementView}: InspectorProps) => {
 
-    const [textValue, setTextValue] = useState(elementView.model.attr("label/text"))
+    const inspectedElementText = elementView.model.attr("label/text");
+    const inspectedElementPortsIn = elementView.model.prop("ports/groups/in/position/name");
+    const inspectedElementPortsOut = elementView.model.prop("ports/groups/out/position/name");
 
-    const handleChange = (event: any) => {
+    const [textValue, setTextValue] = useState(inspectedElementText);
+    const [portsInValue, setPortsInValue] = useState(inspectedElementPortsIn);
+    const [portsOutValue, setPortsOutValue] = useState(inspectedElementPortsOut);
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        elementView.model.attr("label/textWrap", {
+            width: getShapeLabelWidth(elementView),
+            height: 20,
+            ellipsis: true
+        })
+
         setTextValue(event.target.value);
         elementView.model.attr("label/text", event.target.value);
     }
 
+    const handleChangePortIn = (event: SelectChangeEvent) => {
+        setPortsInValue(event.target.value);
+        elementView.model.prop("ports/groups/in/position/name", event.target.value);
+    }
+
+    const handleChangePortOut = (event: SelectChangeEvent) => {
+        setPortsOutValue(event.target.value);
+        elementView.model.prop("ports/groups/out/position/name", event.target.value);
+    }
+
+    useEffect(() => {
+        setTextValue(inspectedElementText);
+        setPortsInValue(inspectedElementPortsIn);
+        setPortsOutValue(inspectedElementPortsOut);
+    }, [inspectedElementText, inspectedElementPortsIn, inspectedElementPortsOut])
+
     return (
         <div className={styles.inspector}>
-            <List
-                sx={{
-                    width: "100%",
-                    bgcolor: '#c9ba91',
-                }}
-                component="nav"
-                aria-labelledby="nested-list-subheader"
-                subheader={
-                    <ListSubheader component="div" id="nested-list-subheader"
-                                   sx={{
-                                       bgcolor: "#B68839",
-                                       width: "100%",
-                                       fontWeight: "bold",
-                                   }}>
-                        Text
-                    </ListSubheader>
-                }
+            <div className={styles.inspectorCategoryContainer}>
+                <h4 className={styles.inspectorCategoryHeader}>Label</h4>
+            </div>
+            <Input className={styles.input} value={textValue} onChange={handleInputChange}/>
+            <div className={styles.inspectorCategoryContainer}>
+                <h4 className={styles.inspectorCategoryHeader}>Port placement</h4>
+            </div>
+            <h5 className={styles.portsSubheader}>Ports In</h5>
+            <Select
+                id="ports-in-select"
+                value={portsInValue}
+                label={portsInValue}
+                onChange={handleChangePortIn}
             >
-                <Input value={textValue} onChange={handleChange}/>
-            </List>
+                <MenuItem value={"left"}>Left</MenuItem>
+                <MenuItem value={"top"}>Top</MenuItem>
+                <MenuItem value={"right"}>Right</MenuItem>
+                <MenuItem value={"bottom"}>Bottom</MenuItem>
+            </Select>
+            <h5 className={styles.portsSubheader}>Ports Out</h5>
+            <Select
+                id="ports-out-select"
+                value={portsOutValue}
+                label={portsOutValue}
+                onChange={handleChangePortOut}
+            >
+                <MenuItem value={"left"}>Left</MenuItem>
+                <MenuItem value={"top"}>Top</MenuItem>
+                <MenuItem value={"right"}>Right</MenuItem>
+                <MenuItem value={"bottom"}>Bottom</MenuItem>
+            </Select>
         </div>
     )
 }
