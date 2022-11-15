e23d7efabca7eb07223901e9d7a0312b7ef0748c
Ideekay12 <48804783+Ideekay12@users.noreply.github.com>
2022-11-15 23:02:10 +0100

    feat(): update store

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -2,12 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 import { diagramsReducer } from "./getDiagramsSlice";
 import { singleDiagramReducer } from "./singleDiagramSlice";
 import { addDiagramReducer } from "./addDiagramSlice";
+import { deleteDiagramReducer } from "./deleteDiagramSlice";
+import { updateDiagramReducer } from "./updateDiagramSlice";
 
 const store = configureStore({
     reducer: {
         diagrams: diagramsReducer,
         singleDiagram: singleDiagramReducer,
-        addDiagram: addDiagramReducer
+        addDiagram: addDiagramReducer,
+        deleteDiagram: deleteDiagramReducer,
+        updateDiagram: updateDiagramReducer
     }
 });
 
