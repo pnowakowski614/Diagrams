1a4fdebe5571e9c24491cbb94b572cb0ce1421ae
Ideekay12 <48804783+Ideekay12@users.noreply.github.com>
2022-11-26 04:54:16 +0100

    feat(): add signup form validation

--- src/app/API/fetchMethods.ts
+++ src/app/API/fetchMethods.ts
@@ -1,84 +1,139 @@
-const callApiMethod = (url: string, methodName: string, body?: BodyInit, headers?: HeadersInit): Promise<any> | void => {
-    return fetch(url, {
-        method: methodName,
-        headers: headers,
-        body: body
-    }).then(response => {
-            return response.json()
-        }
-    ).catch((error) => {
-        alert("Error!: " + error.toString());
-    })
-}
+import { Routes } from "../types/enums";
 
-export const postToDb = async (diagram: JSON, diagramName: string): Promise<any> => {
-    const body: BodyInit = JSON.stringify({
-        diagram,
-        diagramName
+const callApiMethod = (
+  url: string,
+  methodName: string,
+  body?: BodyInit,
+  headers?: HeadersInit
+): Promise<any> | void => {
+  return fetch(url, {
+    method: methodName,
+    headers: headers,
+    body: body,
+  })
+    .then((response) => {
+      return response.json();
     })
-    return callApiMethod(`${process.env.REACT_APP_BACKEND_URL}/diagrams`, 'POST',
-        body, {'Content-Type': 'application/json', "x-access-token": localStorage.getItem('token')!})
-}
+    .catch((error) => {
+      alert("Error!: " + error.toString());
+    });
+};
+
+export const postToDb = async (
+  diagram: JSON,
+  diagramName: string
+): Promise<any> => {
+  const body: BodyInit = JSON.stringify({
+    diagram,
+    diagramName,
+  });
+  return callApiMethod(
+    `${process.env.REACT_APP_BACKEND_URL}/diagrams`,
+    "POST",
+    body,
+    {
+      "Content-Type": "application/json",
+      "x-access-token": localStorage.getItem("token")!,
+    }
+  );
+};
 
 export const getDiagramListFromDb = async (): Promise<any> => {
-    const headers: HeadersInit = {
-        "x-access-token": localStorage.getItem('token') ?? ""
-    }
+  const headers: HeadersInit = {
+    "x-access-token": localStorage.getItem("token") ?? "",
+  };
 
-    return callApiMethod(`${process.env.REACT_APP_BACKEND_URL}/diagrams`, 'GET', undefined, headers);
-}
+  return callApiMethod(
+    `${process.env.REACT_APP_BACKEND_URL}/diagrams`,
+    "GET",
+    undefined,
+    headers
+  );
+};
 
 export const getSingleDiagramFromDb = async (id: string): Promise<any> => {
-    return callApiMethod(`${process.env.REACT_APP_BACKEND_URL}/diagrams/${id}`, 'GET', undefined, {'Content-Type': 'application/json'});
-}
+  return callApiMethod(
+    `${process.env.REACT_APP_BACKEND_URL}/diagrams/${id}`,
+    "GET",
+    undefined,
+    { "Content-Type": "application/json" }
+  );
+};
 
-export const updateDiagramInDb = async (cells: JSON, diagramName: string, id: string): Promise<any> => {
-    const body: BodyInit = JSON.stringify({
-        cells,
-        diagramName
-    })
+export const updateDiagramInDb = async (
+  cells: JSON,
+  diagramName: string,
+  id: string
+): Promise<any> => {
+  const body: BodyInit = JSON.stringify({
+    cells,
+    diagramName,
+  });
 
-    return callApiMethod(`${process.env.REACT_APP_BACKEND_URL}/diagrams/${id}`, 'PATCH',
-        body, {'Content-Type': 'application/json'})
-}
+  return callApiMethod(
+    `${process.env.REACT_APP_BACKEND_URL}/diagrams/${id}`,
+    "PATCH",
+    body,
+    { "Content-Type": "application/json" }
+  );
+};
 
 export const deleteFromDb = async (id: string): Promise<any> => {
-    return callApiMethod(`${process.env.REACT_APP_BACKEND_URL}/diagrams/${id}`, 'DELETE')
-}
+  return callApiMethod(
+    `${process.env.REACT_APP_BACKEND_URL}/diagrams/${id}`,
+    "DELETE"
+  );
+};
 
-export const loginUser = async (username: string, password: string): Promise<any> => {
-    const body = JSON.stringify({
-        username,
-        password
-    })
+export const loginUser = async (
+  username: string,
+  password: string
+): Promise<any> => {
+  const body = JSON.stringify({
+    username,
+    password,
+  });
 
-    const data = await callApiMethod(`${process.env.REACT_APP_BACKEND_URL}/users/login`, 'POST',
-        body, {'Content-Type': 'application/json'})
+  const data = await callApiMethod(
+    `${process.env.REACT_APP_BACKEND_URL}/users/login`,
+    "POST",
+    body,
+    { "Content-Type": "application/json" }
+  );
 
-    if (data.user) {
-        localStorage.setItem('token', data.user);
-        alert("Login successful!");
-        window.location.href = "/diagram";
-    } else {
-        alert("Incorrect username or password!");
-    }
+  if (data.user) {
+    localStorage.setItem("token", data.user);
+    alert("Login successful!");
+    window.location.href = Routes.diagram;
+  } else {
+    alert("Incorrect username or password!");
+  }
 
-    return data;
-}
+  return data;
+};
 
-export const registerUser = async (username: string, password: string, email: string): Promise<any> => {
-    const body = JSON.stringify({
-        username,
-        email,
-        password
-    })
+export const registerUser = async (
+  username: string,
+  password: string,
+  email: string
+): Promise<any> => {
+  const body = JSON.stringify({
+    username,
+    email,
+    password,
+  });
 
-    const data = await callApiMethod(`${process.env.REACT_APP_BACKEND_URL}/users/register`, 'POST',
-        body, {'Content-Type': 'application/json'})
+  const data = await callApiMethod(
+    `${process.env.REACT_APP_BACKEND_URL}/users/register`,
+    "POST",
+    body,
+    { "Content-Type": "application/json" }
+  );
 
-    if (data.status === 'ok') {
-        window.location.href = "/login";
-    } else {
-        alert("Registration unsuccessful!")
-    }
-}
+  if (data.status === "ok") {
+    localStorage.setItem("token", data.user);
+    window.location.href = Routes.diagram;
+  } else {
+    alert("Registration unsuccessful!");
+  }
+};
--- src/app/views/SignUp/SignUpForm.tsx
+++ src/app/views/SignUp/SignUpForm.tsx
@@ -1,39 +1,97 @@
-import React, { ChangeEvent, useState } from 'react';
-import '../Login/login.scss';
+import React, { ChangeEvent, useState } from "react";
+import "../Login/login.scss";
 import { Routes } from "../../types/enums";
 import FormInput from "../Login/FormInput";
 import { registerUser } from "../../API/fetchMethods";
 
 const SignUpForm = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [email, setEmail] = useState("");
+  const form = document.getElementById("form-container") as HTMLSelectElement;
 
-    return (
-        <form className="form-container">
-            <h1 className="login-header">Create Your Account</h1>
-            <div className="inputs">
-                <FormInput type="text" placeholder="Username"
-                           onChange={(e: ChangeEvent<HTMLInputElement>) => setUsername(e.target.value)}/>
-                <FormInput type="email" placeholder="E-mail"
-                           onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}/>
-                <FormInput type="password" placeholder="Password"
-                           onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}/>
-            </div>
-            <div className="continue-button-container">
-                <button className="continue-button" type="button"
-                        onClick={(e) => {
-                            e.preventDefault();
-                            registerUser(username, password, email);
-                        }}>Sign
-                    Up
-                </button>
-            </div>
-            <p className="change-mode-message">Already have an account?
-                <a id="change-mode-link" href={Routes.login}>Log In</a>
-            </p>
-        </form>
-    )
-}
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSubmit = (e: React.MouseEvent) => {
+    e.preventDefault();
+    if (form) {
+      if (form.checkValidity()) {
+        registerUser(username, password, email);
+      } else {
+        alert("Invalid form!");
+      }
+    } else {
+      alert("Invalid form!");
+    }
+  };
+
+  return (
+    <form id="form-container">
+      <h1 className="login-header">Create Your Account</h1>
+      <div className="inputs">
+        <FormInput
+          type="text"
+          name="username"
+          placeholder="Username"
+          errorMessage="Username should be 3-10 characters long and shouldn't include any special characters!"
+          pattern="^[A-Za-z0-9]{3,10}$"
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setUsername(e.target.value)
+          }
+          required={true}
+        />
+        <FormInput
+          type="email"
+          name="email"
+          placeholder="E-mail"
+          errorMessage="Invalid e-mail!"
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setEmail(e.target.value)
+          }
+          required={true}
+        />
+        <FormInput
+          type="password"
+          name="password"
+          placeholder="Password"
+          errorMessage="Password should be 8-20 characters long and include at least 1 letter, 1 number and 1 special character!"
+          pattern="^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$"
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setPassword(e.target.value)
+          }
+          required={true}
+        />
+        <FormInput
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirm Password"
+          errorMessage="Passwords don't match!"
+          pattern={password}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setConfirmPassword(e.target.value)
+          }
+          required={true}
+        />
+      </div>
+      <div className="continue-button-container">
+        <button
+          className="continue-button"
+          type="button"
+          onClick={(e) => {
+            handleSubmit(e);
+          }}
+        >
+          Sign Up
+        </button>
+      </div>
+      <p className="change-mode-message">
+        Already have an account?
+        <a id="change-mode-link" href={Routes.login}>
+          Log In
+        </a>
+      </p>
+    </form>
+  );
+};
 
 export default SignUpForm;
