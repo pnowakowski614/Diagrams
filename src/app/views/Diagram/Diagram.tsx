93ebe5579f8163430049c152758764ce58cc9352
Ideekay12 <48804783+Ideekay12@users.noreply.github.com>
2022-12-18 23:39:14 +0100

    feat(): zoom to fit on open

--- src/app/views/Diagram/Diagram.tsx
+++ src/app/views/Diagram/Diagram.tsx
@@ -69,6 +69,9 @@ const Diagram = () => {
           dispatch(getSingleDiagram(diagramId));
         }
       }
+      if (currentDiagram?.length) {
+        rappidInst.scroller.zoomToFit();
+      }
     }
   });
 
